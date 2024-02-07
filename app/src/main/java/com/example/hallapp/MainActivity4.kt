645013c8c0392d3fe135cfda169701d1f2968f2a
rajib.das <rajibrobidas22222@gmail.com>
2024-02-07 13:23:56 +0600

    Second commit

--- app/src/main/java/com/example/hallapp/MainActivity4.kt
+++ app/src/main/java/com/example/hallapp/MainActivity4.kt
@@ -4,19 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 
 class MainActivity4 : AppCompatActivity() {
 
-    lateinit var authority : TextView
-    lateinit var notice : TextView
-    lateinit var office : TextView
-    lateinit var students : TextView
-    lateinit var foods : TextView
-    lateinit var medicin : TextView
-    lateinit var services : TextView
-    lateinit var complaints : TextView
-    lateinit var sports : TextView
-    lateinit var contacts : TextView
+    lateinit var authority : CardView
+    lateinit var notice : CardView
+    lateinit var office : CardView
+    lateinit var students : CardView
+    lateinit var foods : CardView
+    lateinit var medicin : CardView
+    lateinit var services : CardView
+    lateinit var complaints : CardView
+    lateinit var sports : CardView
+    lateinit var contacts : CardView
     lateinit var arback : TextView
 
 
@@ -30,55 +31,55 @@ class MainActivity4 : AppCompatActivity() {
             startActivity(intent)
         }
 
-        authority = findViewById(R.id.authority)
+        authority = findViewById(R.id.autho)
         authority.setOnClickListener {
             val intent = Intent(this@MainActivity4, MainActivity14::class.java)
             startActivity(intent)        }
 
-        notice = findViewById(R.id.notice)
+        notice = findViewById(R.id.noti)
         notice.setOnClickListener {
             val intent = Intent(this@MainActivity4, MainActivity15::class.java)
             startActivity(intent)        }
 
-        office = findViewById(R.id.office)
+        office = findViewById(R.id.offi)
         office.setOnClickListener {
             val intent = Intent(this@MainActivity4, MainActivity16::class.java)
             startActivity(intent)        }
 
-        students = findViewById(R.id.students)
+        students = findViewById(R.id.stu)
         students.setOnClickListener {
             val intent = Intent(this@MainActivity4, MainActivity12::class.java)
             startActivity(intent)
         }
 
-        foods = findViewById(R.id.foods)
+        foods = findViewById(R.id.foo)
         foods.setOnClickListener {
             val intent = Intent(this@MainActivity4, MainActivity5::class.java)
             startActivity(intent)
         }
 
-        medicin = findViewById(R.id.medicin)
+        medicin = findViewById(R.id.medi)
         medicin.setOnClickListener {
             val intent = Intent(this@MainActivity4, MainActivity8::class.java)
             startActivity(intent)
         }
 
-        services = findViewById(R.id.services)
+        services = findViewById(R.id.serv)
         services.setOnClickListener {
             val intent = Intent(this@MainActivity4, MainActivity13::class.java)
             startActivity(intent)        }
 
-        complaints = findViewById(R.id.complaints)
+        complaints = findViewById(R.id.comp)
         complaints.setOnClickListener {
             val intent = Intent(this@MainActivity4, MainActivity11::class.java)
             startActivity(intent)        }
 
-        sports = findViewById(R.id.sports)
+        sports = findViewById(R.id.spo)
         sports.setOnClickListener {
             val intent = Intent(this@MainActivity4, MainActivity9::class.java)
             startActivity(intent)        }
 
-        contacts = findViewById(R.id.contacts)
+        contacts = findViewById(R.id.con)
         contacts.setOnClickListener {
             val intent = Intent(this@MainActivity4, MainActivity10::class.java)
             startActivity(intent)        }
